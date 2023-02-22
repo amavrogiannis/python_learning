9215f5b4497a296c0d6c0607a43bb60d053b5596
Alexander Mavrogiannis <alexander.mavrogiannis@and.digital>
2023-02-22 12:25:42 +0000

    trying to make pylint happy

--- RPS_game.py
+++ RPS_game.py
@@ -1,26 +1,25 @@
-"""This is a Rock Paper Scissors game"""
+# """This is a Rock Paper Scissors game"""
 import random
-import sys
 
-""" Defining objects """
+# """ Defining objects """
 object_list = ["rock", "paper", "scissors"]
 
-""" Human selected object """
+# """ Human selected object """
 def human_player():
     while True:
         user_input = input("Please input your object 👉 ").lower()
         if user_input in object_list:
             print(f"👤 You selected 👉 {user_input.upper()}")
             return user_input
-        print(f"This object is invalid, please select one of the following: \n {object_list}") 
+        print(f"This object is invalid, please select one of the following:\n {object_list}") 
 
-""" Robot's selected object """
+# """ Robot's selected object """
 def robot_player():
     robot_selection = random.choice(object_list)
     print(f"🤖 Robot selected 👉 {robot_selection.upper()}")
     return robot_selection
 
-""" Decision """
+# """ Decision """
 def game_decision(user_input, robot_selection):
     if user_input == robot_selection:
         print("This round is a DRAW")
@@ -41,10 +40,10 @@ def game_decision(user_input, robot_selection):
             print(f"You WIN! {user_input} cuts {robot_selection}")
 
 def main():
-    """ This is the main function """
+    # """ This is the main function """
     user_input = human_player()
     robot_selection = robot_player()
     game_decision(user_input, robot_selection)
 
-""" Execure main function """
+# """ Execure main function """
 main()
