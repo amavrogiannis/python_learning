300507b812a6d6be84769f3d3e22829be82b4cc3
Alexander Mavrogiannis <alexander.mavrogiannis@and.digital>
2023-03-30 18:25:38 +0100

    adding code with class

--- RPS_game_oop.py
+++ RPS_game_oop.py
@@ -5,11 +5,26 @@ import sys
 
 # """ Defining objects """
 GAME_OBJECTS = ["rock", "paper", "scissors"]
-CHOISE = ["y", "n"]
+CHOICE = ["y", "n"]
 
 class Scoreboard:
-    human_score = 0
-    robot_score = 0
+    def __init__(self, human_score = 0, robot_score = 0):
+        self.human_score = human_score
+        self.robot_score = robot_score
+
+    def add_human_score(self):
+        self.human_score += 3
+
+    def add_robot_score(self):
+        self.robot_score += 3
+
+    def draw(self):
+        self.human_score += 1
+        self.robot_score += 1
+
+    def game_scores(self):
+        print("GAME SUMMARY:")
+        return f"Human: {self.human_score}\nComputer: {self.robot_score}"
 
 """ Human selected object """
 def human_player():
@@ -30,70 +45,63 @@ def robot_player():
     return robot_selection
 
 # """ This function writen the game decision """
-def game_decision(user_input, robot_selection):
-    global human_score, robot_score
+def game_decision(scoreboard, user_input, robot_selection):
     """ This generator is set to decide who wins according to human vs computer selected object """
     if user_input == robot_selection:
         print("This round is a DRAW")
         # score value on resulting draw
-        Scoreboard.human_score += 1
-        Scoreboard.robot_score += 1
+        scoreboard.draw()
     elif user_input == GAME_OBJECTS[0]:
         if robot_selection == GAME_OBJECTS[1]:
             print(f"You LOSE! {robot_selection} covers {user_input}")
             # score value on win result
-            Scoreboard.robot_score += 3
+            scoreboard.add_robot_score()
         else:
             print(f"You WIN! {user_input} smashes {robot_selection}")
             # score value on win result
-            Scoreboard.human_score += 3
+            scoreboard.add_human_score()
     elif user_input == GAME_OBJECTS[1]:
         if robot_selection == GAME_OBJECTS[2]:
             print(f"You LOSE! {robot_selection} cuts {user_input}")
             # score value on win result
-            Scoreboard.robot_score += 3
+            scoreboard.add_robot_score()
         else:
             print(f"You WIN! {user_input} covers {robot_selection}")
             # score value on win result
-            Scoreboard.human_score += 3
+            scoreboard.add_human_score()
     elif user_input == GAME_OBJECTS[2]:
         if robot_selection == GAME_OBJECTS[0]:
             print(f"You LOSE! {robot_selection} smashes {user_input}")
             # score value on win result
-            Scoreboard.robot_score += 3
+            scoreboard.add_robot_score()
         else:
             print(f"You WIN! {user_input} cuts {robot_selection}")
             # score value on win result
-            Scoreboard.human_score += 3
+            scoreboard.add_human_score()
 
 # """ This function allows the user to resume the game """
 def player_resume():
     """ Ask the user to resume the game with "y" or "n" options """
     while True:
         resume_game = input("Do you want to resume play? y/n: ").lower()
-        if resume_game in CHOISE:
+        if resume_game in CHOICE:
             if resume_game == "y":
                 break
             if resume_game == "n":
                 sys.exit()
-        print(f"This selection is invalid, need to be {CHOISE}")
-
-# """ Score summary of the game """
-def game_scores():
-    print("GAME SUMMARY:")
-    print(f"Human: {Scoreboard.human_score}")
-    print(f"Computer: {Scoreboard.robot_score}")
-
-
+        print(f"This selection is invalid, need to be {CHOICE}")
 
 def main():
     """ This is the main function which pull other functions """
     while True:
-        user_input = human_player()
-        robot_selection = robot_player()
-        game_decision(user_input, robot_selection)
-        game_scores()
-        player_resume()
+        scoreboard = Scoreboard()
+        while True:
+            user_input = human_player()
+            robot_selection = robot_player()
+            game_decision(scoreboard, user_input, robot_selection)
+            print(scoreboard.game_scores())
+            player_resume()
 
 # pylint: disable=C0304
-main()
+if __name__ == '__main__':
+    main()
