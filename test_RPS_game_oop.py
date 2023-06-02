4e6f652084ef57b956018505dad74679d1327add
Alexander Mavrogiannis <alexander.mavrogiannis@and.digital>
2023-06-02 01:17:53 +0100

    writing test file

--- test_RPS_game_oop.py
+++ test_RPS_game_oop.py
@@ -1,13 +1,36 @@
 from RPS_game_oop import *
+import pytest
 import random
-import sys
+import mock
+import builtins
 
-# Variables
-EXPECTED_OBJECTS = ["rock", "paper", "scissors"]
+GAME_OBJECTS = ["rock", "paper", "scissors"]
 
-def test_robot_player():
-    result = robot_player()
-    assert result in EXPECTED_OBJECTS
+def test_human_player():
+    test_var = random.choice(GAME_OBJECTS)
+    with mock.patch.object(builtins, 'input', lambda _: test_var):
+        if test_var in GAME_OBJECTS:
+            assert human_player() == test_var
+        else: 
+            exit(1)
 
-def game_decision():
-    
+def test_init_score():
+    scoreboard = Scoreboard()
+    assert scoreboard.human_score == 0
+    assert scoreboard.robot_score == 0
+
+def test_add_human_score():
+    scoreboard = Scoreboard()
+    scoreboard.add_human_score()
+    assert scoreboard.human_score == 3
+
+def test_add_robot_score():
+    scoreboard = Scoreboard()
+    scoreboard.add_robot_score()
+    assert scoreboard.robot_score == 3
+
+def test_draw():
+    scoreboard = Scoreboard()
+    scoreboard.draw()
+    assert scoreboard.human_score == 1
+    assert scoreboard.robot_score == 1
