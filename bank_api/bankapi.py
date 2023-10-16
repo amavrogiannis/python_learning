7e8ca2c4a1746f32df523d49c12ee815d3ccfa76
Alexander Mavrogiannis <alexander.mavrogiannis@and.digital>
2023-10-17 00:07:48 +0100

    building deposit class for the bank api

--- bank_api/bankapi.py
+++ bank_api/bankapi.py
@@ -3,6 +3,7 @@
 Bank account API
 """
 import json
+from time import gmtime, strftime
 from flask import Flask, request
 from flask_restful import Resource, Api, reqparse
 from account import BankAccount
@@ -11,39 +12,59 @@ from account import BankAccount
 app = Flask(__name__)
 api = Api(app)
 
+file = "accounts.json"
+
+gmt_time = strftime("%d/%m/%Y %H:%M:%S", gmtime())
+
 class AccountID(Resource):
     """ API Class for Bank Account """
 
-    def get(self, account_id:int, balance:int=1200):
+    def get(self, account_no:int):
         """ Applying AccountID and Balance """
-        data = BankAccount(account_id, balance)
-        data.account_id = account_id
-        data.balance = balance
-        self.transactions =[]
-        json_list = json.dump(self.transactions)
-        return {
-            'account_id': f'{data.account_id}',
-            'balance': f'{data.balance}',
-            'transactions': f[{json_list}]
-        }
+        with open(file, 'r') as bank_file: 
+            transactions_data = json.load(bank_file)
 
-    # def post(self):
-    #     bank = BankAccount()
-    #     """ This method will update the JSON file """
-    #     data = request.get_json()
-    #     deposit = data['deposit']
-    #     balance = bank.deposit(deposit)
 
-    #     return {
-    #         'deposit': f'{deposit}'
-    #     }
+        for items in transactions_data:
+            if 'account_id' in items and items['account_id'] == account_no:
+                results = app.response_class(
+                    response=json.dumps(items, indent=4),
+                    status=200,
+                    mimetype='application/json'
+                )
+
+        return results
+    
+class Deposit(Resource):
+    def post(self, account_no:int, amount:int):
+        with open(file, 'r') as deposit:
+            data = json.load(deposit)
+        
+        for item in data:
+            if "transactions" in item and isinstance(item["transactions"], list):
+                for transaction_item in item["transactions"]:
+                    if "balance" in transaction_item and transaction_item["find"] != None:
+                        new_balance = transaction_item["balance"] + amount
+                        transaction_item["time"] = f'{gmt_time}'
+                        transaction_item["amount"] = f'{amount}'
+                        transaction_item["balance"] = f'{new_balance}'
+
+        with open('your_file.json', 'w') as f:
+            json.dump(data, f, indent=4)
+        
+        results = app.response_class(
+            response={'message':f'Deposited {amount} to {account_no}'},
+            status=200,
+            mimetype='application/json'
+        )
+
 
 # Endpoint 1 
-api.add_resource(AccountID, "/account/<int:account_id>", endpoint= "account")
+api.add_resource(AccountID, "/account/<int:account_no>", endpoint= "account")
 # copy: http://127.0.0.1:5000/account/1002
 
 # Endpoint 2
-# api.add_resource(AccountID, "/account/<int:account_id>/deposit/<int:ammount>", endpoint= "deposit")
+api.add_resource(Deposit, "/account/<int:account_no>/deposit/<int:amount>", endpoint= "deposit")
 # copy: http://127.0.0.1:5000/account/1002/deposit/100
 
 if __name__ == "__main__":
